1dfe76c0f41f38efbe75a64d46707b5abb243d44
Lucas Leiberman <dr.lucasleibs@gmail.com>
2020-04-25 19:36:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -43,7 +43,7 @@ produce_storage = [
 end
 
 def matrix_lookup(matrix, row, column)
- return assembled_matrix[1][1]
+ matrix[column][row]
  
   # Given any matrix (array of arrays), a row index and a column index, 
   # Return the matrix's content at that row and and column
